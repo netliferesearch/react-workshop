754744fb42777dee33c80fd52680015d4b56d42b
Kristoffer Brabrand <kristoffer@brabrand.no>
2016-08-08 21:24:58 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index.jsx',
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/',
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loaders: ['react-hot', 'babel'],
+            include: path.join(__dirname, 'src'),
+        }],
+    },
+};
