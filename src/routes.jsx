c34ac5aadd75c207d03ff572e2a7ce7c9d53d85c
Kristoffer Brabrand <kristoffer@brabrand.no>
2016-08-18 15:50:15 +0200

    Get rid of key in hash

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,15 @@
-
 import React from 'react';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import createHashHistory from 'history/lib/createHashHistory';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 
 import Main from './components/Main';
 import Home from './pages/Home';
 import ErrorPage from './pages/ErrorPage';
 
-const history = createHashHistory({ queryKey: false });
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 const routes = (
-    <Router history={history}>
+    <Router history={appHistory}>
         <Route path="/" component={Main} >
             <IndexRoute component={Home} />
         </Route>
